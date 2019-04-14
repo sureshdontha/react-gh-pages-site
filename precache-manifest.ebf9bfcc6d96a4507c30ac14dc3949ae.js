d6717a133a229ef2e6204ff8db816e08e501b8c5
sureshdontha <sureshdontha@hotmail.com>
2019-04-13 23:54:37 -0400

    Updates

---
+++ precache-manifest.ebf9bfcc6d96a4507c30ac14dc3949ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e43eac98c5944aa2f9f",
+    "url": "/react-gh-pages-site/static/css/main.ce8c0d5b.chunk.css"
+  },
+  {
+    "revision": "8e43eac98c5944aa2f9f",
+    "url": "/react-gh-pages-site/static/js/main.922c371c.chunk.js"
+  },
+  {
+    "revision": "f92c2bfabf178fc79f49",
+    "url": "/react-gh-pages-site/static/js/runtime~main.4b5b7b03.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-gh-pages-site/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages-site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4ae04df6f2e7a262fb64ef81268ac334",
+    "url": "/react-gh-pages-site/index.html"
+  }
+];
